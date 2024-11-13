70cceecfe2bc7e1d036a18adf2a785738ea2a1af
TABARY Angel-Remy <Angel-Remy.Tabary@bordeaux-inp.fr>
2024-11-13 16:39:55 +0100

    Réalisation de la classe Player

--- Player.java
+++ Player.java
@@ -1,5 +1,50 @@
-public class Player {
+public class Player 
+{
+    // Attributs
+    int playerId;
     String name; 
-    // Liste de Card 
-    // Liste des frontières gagnées 
+    private List<Card> hand;
+    private List<Borne> wonBorders;
+
+    // Constructeur
+    public Player(String name) {
+        this.name = name;
+        this.hand = new ArrayList<>();      
+        this.wonBorders = new ArrayList<>(); 
+    }
+
+    // Methodes
+    public void addCardToHand(Card card) {
+        hand.add(card);
+    }
+    
+    public void removeCardFromHand(Card card) {
+        hand.remove(card);
+    }
+
+    public void addWonBorder(Border border) {
+        wonBorders.add(border);
+    }
+    
+    public List<Borne> getWonBorders() {
+        return wonBorders;
+    }
+    
+    // Setters et getters
+    public String setName(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String setHand(String hand) {
+        this.hand = hand;
+    }
+
+    public String getHand() {
+        return hand;
+    }
+
 }
