f137c898b6c565466a8b4bbe43d7d94b11ad7a51
Elisa Chien <echien@bordeaux-inp.fr>
2024-12-12 22:39:27 +0100

    Correction d'affichage

--- Board.java
+++ Board.java
@@ -193,7 +193,7 @@ public class Board {
             }
             else
             {
-                System.out.print("  ");
+                System.out.print("     ");
             }
         }
 
@@ -208,7 +208,7 @@ public class Board {
             }
             else
             {
-                System.out.print("      "); 
+                System.out.print("     "); 
             }
         }
         
@@ -223,7 +223,7 @@ public class Board {
             }
             else
             {
-                System.out.print("      ");
+                System.out.print("     ");
             }
         }
 
